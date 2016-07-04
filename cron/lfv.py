44021d192f17ab3473ccb83733a2bd108d8098bb
masenius <masenius@gmail.com>
2016-07-04 12:46:27 +0200

    Added some very basic error handling

--- cron/lfv.py
+++ cron/lfv.py
@@ -3,6 +3,9 @@
 import requests
 
 site = requests.get("https://www.aro.lfv.se/Links/Link/ViewLink?TorLinkId=308&type=MET")
-
-f = open('D:/vader/lfv-weather.html', 'w')
-f.write(site.text)
+if site.status_code is 200:
+    try:
+        f = open('D:/vader/lfv-weather.html', 'w')
+        f.write(site.text)
+    except:
+        continue
