1873fbfc5db7d45ffa0a44aaeaa9329c3c2bbc1d
David <masenius@gmail.com>
2017-02-28 12:14:05 +0000

    Write file as UTF-8 to handle the new site

--- cron/lfv.py
+++ cron/lfv.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 import requests
+import codecs
 
 site = requests.get("https://www.aro.lfv.se/Links/Link/ViewLink?TorLinkId=308&type=MET")
 if site.status_code is 200:
     try:
-        f = open('D:/vader/lfv-weather.html', 'w')
+        f = codecs.open('"D:/vader/lfv-weather.html', 'w', 'utf-8')
         f.write(site.text)
     except:
         pass
