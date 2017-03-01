1a83cf66479ac4f28c7179a2f10ff57c619973ce
Linköpings Fallskärmsklubb <nobody@linkopingsfallskarmsklubb.se>
2017-03-01 10:04:02 +0100

    Remove error in path

--- cron/lfv.py
+++ cron/lfv.py
@@ -6,7 +6,7 @@ import codecs
 site = requests.get("https://www.aro.lfv.se/Links/Link/ViewLink?TorLinkId=308&type=MET")
 if site.status_code is 200:
     try:
-        f = codecs.open('"D:/vader/lfv-weather.html', 'w', 'utf-8')
+        f = codecs.open('D:/vader/lfv-weather.html', 'w', 'utf-8')
         f.write(site.text)
     except:
         pass
